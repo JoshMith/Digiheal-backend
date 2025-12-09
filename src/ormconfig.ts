8d5d115de6f6f8d33dceaf8c2d6a857b59ac5081
JoshMith <JoshMith@users.noreply.github.com>
2025-12-09 09:40:24 +0300

    Back to Prisma

--- src/ormconfig.ts
+++
@@ -1,7 +0,0 @@
-// ormconfig.ts
-
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './config/database.config';
-
-// Export default DataSource for TypeORM CLI
-export default new DataSource(dataSourceOptions);
